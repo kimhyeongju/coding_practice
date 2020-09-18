226b6f11bb6cae2bc3dba334c604d2e18ad72ab9
KimHyeongJu <hengzhu1994@gmail.com>
2020-09-18 15:11:55 +0000

    example

--- Python/시간측정.py
+++ Python/시간측정.py
@@ -5,17 +5,17 @@ import time
 
 array = []
 for _ in range(10000):
-  array.append(randint(1,100))
+    array.append(randint(1, 100))
 
 # 선택 정렬
 start_time = time.time()
 
 for i in range(len(array)):
-  min_index = i
-  for j in range(i+1, len(array)):
-      if array[min_index] > array[j]:
-        min_index = j
-  array[i], array[min_index] = array[min_index] , array[i]
+    min_index = i
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]
 
 end_time = time.time()
 print(f"선택 정렬 시간 측정: {end_time - start_time:.5f}초")
