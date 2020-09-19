d4dfcfb7a4bab7645d9c57cdf8a8165cd4073207
repl.it user <replituser@example.com>
2020-09-19 22:49:23 +0000

    큰수의법칙. ans

--- Chapter3(Greedy)/큰 수의 법칙.py
+++ Chapter3(Greedy)/큰 수의 법칙.py
@@ -49,6 +49,29 @@ n, m, k = map(int, input().split())
 # N개의 수를 공백으로 구분하여 입력받기
 data = list(map(int, input().split()))
 
+# 입력받은 수들 정렬하기
+data.sort()
+first = data[n-1]
+second = data[n-2]
 
+result = 0
 
+while True:
+  for i in range(k):
+    if m == 0:
+      break
+    result += first
+    m -= 1
+  if m == 0:
+    break
+  result += second
+  m -= 1
 
+  print(result)
+
+"""
+
+내가 풀은 것이 효율적인 답안 예시로 나왔음!
+while문 이용하는 방법도 알아두기
+
+"""
