d458750f6deec74f062fc4aba8832f9922ff8410
repl.it user <replituser@example.com>
2020-09-19 22:27:52 +0000

    add appendix

---
+++ APPENDIX/6_library.py
@@ -0,0 +1,17 @@
+# min()과 max()
+min_value = min(2,4,6,7,10)
+max_value = max(2,4,6,7,10)
+print(f"min={min_value}, max={max_value}")
+
+# 수식이 문자열일 경우 사용하는 eval()
+result = eval("2+3*6")
+print(result)
+
+# sort()와 sorted()
+array = [2,6,5,1,7,9,11]
+result1 = sorted(array)
+result2 = sorted(array, reverse=True)
+result3 = array
+array.sort()
+result4 = array
+print(result1, result2, result3, result4, sep="\n")
