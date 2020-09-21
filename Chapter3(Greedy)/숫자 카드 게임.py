8450051f97ee7145392972b6e0b9bcd8b9d4e4f7
kimhyeongju <hengzhu1994@gmail.com>
2020-09-21 12:59:19 +0900

    update

--- Chapter3(Greedy)/숫자 카드 게임.py
+++ Chapter3(Greedy)/숫자 카드 게임.py
@@ -34,15 +34,27 @@ print(row_min_cards)
 
 """
 
-# min() 함수를 이용하는 답안
 import sys
 n, m = map(int,sys.stdin.readline().split())
 result = 0
 
+# min() 함수를 이용하는 답안
 for i in range(n):
     data = list(map(int,sys.stdin.readline().split()))
     # 현재 줄에서 '가장 작은 수' 찾기
     min_value = min(data)
+    # '가장 작은 수'들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
+
+print(result)
+
+# 2중 반복문 구조를 이용하는 답안
+for i in range(n):
+    data = list(map(int,sys.stdin.readline().split()))
+    # 현재 줄에서 '가장 작은 수' 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
     # '가장 작은 수'들 중에서 가장 큰 수 찾기
     result = max(result, min_value)
 
