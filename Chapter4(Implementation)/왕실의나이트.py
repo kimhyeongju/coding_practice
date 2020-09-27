c2490fc83f2ccf19e30c46d79567ac8730e01fb3
kimhyeongju <hengzhu1994@gmail.com>
2020-09-27 23:26:29 +0900

    important!

--- Chapter4(Implementation)/왕실의나이트.py
+++ Chapter4(Implementation)/왕실의나이트.py
@@ -56,4 +56,7 @@ for step in steps:
     # 이동하고자 하는 위치 확인
     next_row = row + step[0]
     next_column = column + step[1]
-    print(next_row,"",next_column)
+    # 해당 위치로 이동 가능하면(값이 0이 아니면) 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
+print(result)
