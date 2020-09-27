9ee4d62829fe3cbbbc70735cb061bc4c62d4b1f6
kimhyeongju <hengzhu1994@gmail.com>
2020-09-27 23:22:46 +0900

    updated

--- Chapter4(Implementation)/왕실의나이트.py
+++ Chapter4(Implementation)/왕실의나이트.py
@@ -46,4 +46,14 @@ print(result)
 input_data = input()
 row = int(input_data[1])
 column = int(ord(input_data[0])) - int(ord('a')) + 1
-print(column)
+
+# 나이트가 이동할 수 있는 8가지 방향 정의
+steps = [(-2,-1),(-2,1),(2,-1),(2,1),(-1,-2),(-1,2),(1,-2),(1,2)]
+
+# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+result = 0
+for step in steps:
+    # 이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    print(next_row,"",next_column)
