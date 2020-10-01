6a393a12f80e7a149a42457f0e15f74374243dc2
kimhyeongju <hengzhu1994@gmail.com>
2020-10-01 21:39:16 +0900

    updated

--- Review/HANGMAN게임(splitlines,pop).py
+++ Review/HANGMAN게임(splitlines,pop).py
@@ -4,13 +4,19 @@ def main():
        ㅣ
        0
       /ㅣ\\
+       ㅣ
       /  \\
     """
     import random
     rnd = random.randint(1,100)
 
     n = HANGMAN.splitlines()
-    # n.pop(0)
-    print(n)
+    n.pop(0)    # 처음 공백 제거
+    # count = 0
+    # for i in n:
+    #     print(i)
+    #     count += 1
+    # print(count)
+
 
 main()
