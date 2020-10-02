e4ea4d0c30f9933aed9e5e5979e62e52c5752494
kimhyeongju <hengzhu1994@gmail.com>
2020-10-02 22:28:24 +0900

    dict_updated

--- Review/dictionary.py
+++ Review/dictionary.py
@@ -1,8 +1,21 @@
-# 사전 관리
 dic = {
     'boy':'소년',
     'school':'학교',
     'book':'책'
 }
-print(dic.get('girl')) # .get(,[]) option의 default는 None
+
+# .get(,[]) option의 default는 None
+print(dic.get('boy'))
+print(dic.get('girl'))
 print(dic.get('girl','사전에 없는 단어입니다.'),'\n')
+
+# 수정, 추가, 삭제
+dic['boy'] = '남자아이' #수정
+dic['girl'] = '소녀' #추가
+del dic['school'] #삭제
+
+# 키, 값 목록 리턴
+print(dic.keys())
+print(dic.values())
+print(dic.items(),'\n') # 튜플 목록 리턴
+
