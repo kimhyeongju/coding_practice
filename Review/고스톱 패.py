8929cf83eb5cf2d2d957c16eedf19d6d75125576
kimhyeongju <hengzhu1994@gmail.com>
2020-10-04 23:42:25 +0900

    updated

--- Review/고스톱 패.py
+++ Review/고스톱 패.py
@@ -1,3 +1,8 @@
+"""
+참가 인원수: user_num
+각자 7장씩 패를 갖는 방식
+"""
+
 import random
 
 # 패 섞기(shuffle)
@@ -6,3 +11,10 @@ def init(user_num):
     random.shuffle(deck)
     users = [[] for _ in range(user_num)]   # comprehension을 이용하여 이중리스트 생성
     return deck, users
+
+# 패 분배
+def assign(users, deck):
+    for _ in range(7):
+        for user_card in users:
+            card = deck.pop(0)
+            user_card.append(card)
