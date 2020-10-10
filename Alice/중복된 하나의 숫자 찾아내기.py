9e905f794a6814d6c14018fb74d2b120bcab2f35
kimhyeongju <hengzhu1994@gmail.com>
2020-10-10 22:00:07 +0900

    updated

--- Alice/중복된 하나의 숫자 찾아내기.py
+++ Alice/중복된 하나의 숫자 찾아내기.py
@@ -5,7 +5,10 @@
 """
 
 def findDuplicate(nums):
-    pass
+    nums.sort()
+    for i in range(len(nums)):
+        if nums[i] != i+1:
+            return nums[i]
 
 def main():
     print(findDuplicate([1, 5, 2, 4, 5, 6, 3]))
