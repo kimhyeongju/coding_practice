22ed65304a81152b3a045054052c16e779cf05dd
kimhyeongju <hengzhu1994@gmail.com>
2020-10-13 23:53:03 +0900

    updated

--- Alice/가장 큰 부분합 구하기.py
+++ Alice/가장 큰 부분합 구하기.py
@@ -5,11 +5,23 @@
 예를 들어, [-10, -7, 5, -7, 10, 5, -2, 17, -25, 1]이 입력으로 들어왔다면
 [10, 5, -2, 17]을 모두 더한 30이 정답이 됩니다.
 """
+import numpy as np
+
 def maxSubArray(nums):
-    return 0
+    arr = []
+    for i in range(len(nums)):
+        subArray = np.cumsum(nums[i:])
+        result = max(subArray)
+        arr.append(result)
+
+    return max(arr)
 
 def main():
     print(maxSubArray([-10, -7, 5, -7, 10, 5, -2, 17, -25, 1])) # 30이 리턴되어야 합니다
+
+    # test_array = [1,2,3]
+    # test = np.cumsum(test_array)
+    # print(test)
 
 if __name__ == "__main__":
     main()
