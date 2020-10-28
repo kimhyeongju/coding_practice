9e4e9c4e8e864e29fb7cd05df1f6a2465a6145f5
kimhyeongju <hengzhu1994@gmail.com>
2020-10-28 18:10:30 +0900

    new

---
+++ Alice/chapter4/팩토리얼 계산하기.py
@@ -0,0 +1,19 @@
+"""
+팩토리얼(!) 은 하나의 정수를 n을 입력받고 n X n-1 X n-2 X …. X 1 을 반환하는 연산자입니다.
+
+예를 들어서, 5! = 5 X 4 X 3 X 2 X 1 = 120 입니다.
+
+팩토리얼 연산자를 파이선 함수로 구현 해 봅시다. 재귀(recursion)방법과 반복(iteration)방법의 두 가지 다른 방법으로 구현 해 보도록 합시다.
+
+1! = 1, 0! = 1 입니다.
+입력값은 0보다 크거나 같은 정수라고 가정합시다.
+"""
+
+def factorial(num):
+    return 0
+
+def main():
+    print(factorial(5)) # should return 120
+
+if __name__ == "__main__":
+    main()
