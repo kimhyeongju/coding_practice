9c30d32ff526d88442f2da611845f10ed57e9072
kimhyeongju <hengzhu1994@gmail.com>
2020-10-31 21:54:49 +0900

    new

---
+++ Alice/chapter6~/연속부분최대합 (Medium)_ans.py
@@ -0,0 +1,38 @@
+import sys
+
+
+def getSubsum(data):
+    n = len(data)
+
+    if n == 1:
+        return data[0]
+
+    mid = n // 2
+
+    left = getSubsum(data[:mid])
+    right = getSubsum(data[mid:])
+
+    Sum = 0
+
+    leftSum = 0
+    rightSum = 0
+
+    for i in range(mid - 1, -1, -1):
+        Sum += data[i]
+        leftSum = max(Sum, leftSum)
+
+    Sum = 0
+
+    for i in range(mid, n):
+        Sum += data[i]
+        rightSum = max(Sum, rightSum)
+
+    return max([left, right, leftSum + rightSum])
+
+def main():
+    data = [int(x) for x in input().split()]
+
+    print(getSubsum(data))
+
+if __name__ == "__main__":
+    main()
