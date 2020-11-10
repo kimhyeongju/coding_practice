ecfa91791d5680277f60cb93eb754b2f5d87af3d
unknown <hengzhu1994@gmail.com>
2020-11-10 13:56:37 +0900

    MNIST

---
+++ DeepLearning/MNIST.py
@@ -0,0 +1,14 @@
+from keras.datasets import mnist
+import numpy as np
+import matplotlib.pyplot as plt
+
+(x_train_data, t_train_data), (x_test_data, t_test_data) = mnist.load_data()
+
+
+print(x_train_data.shape, t_train_data.shape, x_test_data.shape, t_test_data.shape)
+
+test_img = x_train_data[5]
+plt.imshow(test_img)
+plt.show()
+
+
