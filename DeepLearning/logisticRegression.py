98608da8878409bbb16ea49261c3ccca7311f17e
unknown <hengzhu1994@gmail.com>
2020-11-15 22:29:01 +0900

    logistic regression

---
+++ DeepLearning/logisticRegression.py
@@ -0,0 +1,35 @@
+from functions1 import sigmoid
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = [[2, 0], [4, 0], [6, 0], [8, 1], [10, 1], [12, 1],[14, 1]]
+
+x_data = [i[0] for i in data]
+y_data = [i[1] for i in data]
+
+plt.scatter(x_data, y_data)
+plt.xlim(0, 15)
+plt.ylim(-0.1, 1.1)
+# plt.show()
+
+a, b = 0, 0
+lr = 0.05
+
+for i in range(2001):
+    for x_data, y_data in data:
+        a_diff = x_data*(sigmoid(a * x_data + b) - y_data)
+        b_diff = sigmoid(a * x_data + b) - y_data
+
+        a = a - (lr * a_diff)
+        b = b - (lr * b_diff)
+
+    if i % 100 == 0:
+        print(f'epoch={i}, 기울기={a:.04f}, 절편={b:.04f}')
+
+plt.scatter(x_data, y_data)
+plt.xlim(0, 15)
+plt.ylim(-0.1, 1.1)
+x_range = (np.arange(0, 15, 0.1))
+plt.plot(x_range, np.array([sigmoid(a * x + b) for x in x_range]))
+plt.show()
--- DeepLearning/tf2.0_ex03_linearRegression.py
+++ DeepLearning/tf2.0_ex03_linearRegression.py
@@ -5,6 +5,7 @@ from tensorflow.keras.layers import Flatten, Dense
 from tensorflow.keras.optimizers import SGD, Adam
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 x_data = np.array([[1, 2, 0], [5, 4, 3], [1, 2, -1], [3, 1, 0], [2, 4, 2],
                    [4, 1, 2], [-1, 3, 2], [4, 3, 3], [0, 2, 6], [2, 2, 1],
@@ -15,3 +16,33 @@ t_data = np.array([-4, 4, -6, 3, -4,
 
 print('x_data.shape: ', x_data.shape, ', t_data.shape: ', t_data.shape)
 
+# 모델 구축
+model = Sequential()
+model.add(Dense(1, input_shape=(3,), activation='linear'))
+
+# 모델 컴파일
+model.compile(optimizer=SGD(learning_rate=1e-2), loss='mse')
+model.summary()
+
+# 모델 학습
+hist = model.fit(x_data, t_data, epochs=1000)
+
+# 모델 평가 및 예측
+test_data = [[5, 5, 0], [2, 3, 1], [-1, 0, -1], [10, 5, 2], [4, -1, -2]]
+ret_val = [ 2*data[0] -3*data[1] + 2*data[2] for data in test_data ]
+prediction_val = model.predict(np.array(test_data))
+
+print(prediction_val)
+print('====================')
+print(ret_val)
+print(model.weights)
+
+plt.title('Loss Trend')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.grid()
+
+plt.plot(hist.history['loss'], label='train loss')
+plt.legend(loc='best')
+
+plt.show()
