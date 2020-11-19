7e8c2cc66349b41ca7b5954462100e836bd15ad5
unknown <hengzhu1994@gmail.com>
2020-11-19 13:21:49 +0900

    CNN ex02

--- DeepLearning/CNN_ex.py
+++ DeepLearning/CNN_ex.py
@@ -1,16 +1,16 @@
 from keras.datasets import mnist
 import matplotlib.pyplot as plt
-import sys
+import sys, os
 from keras.utils import np_utils
 
 (X_train, Y_class_train), (X_test, Y_class_test) = mnist.load_data()
 print(f"학습셋 이미지 수: {X_train.shape[0]}개")
 print(f"테스트셋 이미지 수: {X_test.shape[0]}개")
 
-plt.imshow(X_train[6], cmap='Greys')
+plt.imshow(X_train[13], cmap='Greys')
 plt.show()
 
-for x in X_train[6]:
+for x in X_train[13]:
     for i in x:
         sys.stdout.write(f"{i}\t")
     sys.stdout.write("\n")
@@ -23,10 +23,10 @@ X_train = X_train / 255
 
 X_test = X_test.reshape(X_test.shape[0], 784).astype('float64') / 255
 
-print(f"class: {Y_class_train[6]}")
+print(f"class: {Y_class_train[13]}")
 
 # one-hot encoding
 Y_train = np_utils.to_categorical(Y_class_train, 10)
 Y_test = np_utils.to_categorical(Y_class_test, 10)
 
-print(Y_train[6])
+print(Y_train[13])
