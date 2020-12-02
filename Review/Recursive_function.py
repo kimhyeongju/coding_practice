e3ca2a346c2dcc35ab175113dc422b3540da1f46
unknown <hengzhu1994@gmail.com>
2020-12-02 21:25:31 +0900

    reculsive review

--- Review/Recursive_function.py
+++ Review/Recursive_function.py
@@ -5,4 +5,20 @@ def reculsive_function(i):
     reculsive_function(i+1)
     print(i, "번째 재귀함수 종료")
 
-reculsive_function(1)
+
+# factorial
+def factorial_iterative(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n-1)
+
+f_i = factorial_iterative(5)
+f_r = factorial_recursive(5)
+print(f_i)
+print(f_r)
