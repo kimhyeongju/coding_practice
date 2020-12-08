abd75f1cad529b5ea7fa928a35cd6a499dab43c3
unknown <hengzhu1994@gmail.com>
2020-12-08 22:07:39 +0900

    openCV

--- Review/openCV_ex01.py
+++ Review/openCV_ex01.py
@@ -1,7 +1,7 @@
 import cv2
 
 image_file = './data/lena.jpg'
-img = cv2.imread(image_file) # cv2.IMREAD_COLOR
+img = cv2.imread(image_file)    # cv2.IMREAD_COLOR
 img2 = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
 
 print(img.shape, img2.shape)
