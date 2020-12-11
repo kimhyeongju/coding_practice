c417d123b7de3d68c979a33cbdf8047578dbd4a4
unknown <hengzhu1994@gmail.com>
2020-12-12 06:56:09 +0900

    액세서

---
+++ Review/액세서.py
@@ -0,0 +1,15 @@
+# getset
+class Date():
+    def __init__(self, month):
+        self.month = month
+
+    def getmonth(self):
+        return self.month
+
+    def setmonth(self, month):
+        if 1 <= month <= 12:
+            self.month = month
+
+today = Date(8)
+today.setmonth(15)
+print(today.getmonth())
