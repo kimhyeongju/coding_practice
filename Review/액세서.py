ccc21d95b18416bed06fec546711b7d8c5ed4da4
unknown <hengzhu1994@gmail.com>
2020-12-12 07:06:31 +0900

    update

--- Review/액세서.py
+++ Review/액세서.py
@@ -13,3 +13,21 @@ class Date():
 today = Date(8)
 today.setmonth(15)
 print(today.getmonth())
+
+# property
+class Score():
+    def __init__(self, math):
+        self.inner_math = math
+
+    def getscore(self):
+        return self.inner_math
+
+    def setscore(self, math):
+        if 0 <= math <= 100:
+            self.inner_math = math
+
+    math = property(getscore, setscore)
+
+score = Score(90)
+score.math = 105    # 허락 안됨
+print(score.math)
