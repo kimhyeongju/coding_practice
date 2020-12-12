478ee771e618a3772f187c6d84dd6096567bb1a2
unknown <hengzhu1994@gmail.com>
2020-12-13 08:07:49 +0900

    clsstr

--- Review/여러 가지 메서드.py
+++ Review/여러 가지 메서드.py
@@ -13,3 +13,14 @@ pride = Car("프라이드")
 korando = Car("코란도")
 Car.outcount()
 
+#clsstr
+class Human:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+
+    def __str__(self):
+        return f"이름: {self.name}, 나이: {self.age}"
+
+kim = Human(30,"김태연")
+print(kim)
