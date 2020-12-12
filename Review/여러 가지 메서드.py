1cee1eefe1dee7a5f695af8f198700a9c7bbfd3b
unknown <hengzhu1994@gmail.com>
2020-12-13 08:03:36 +0900

    classmethod

---
+++ Review/여러 가지 메서드.py
@@ -0,0 +1,15 @@
+# classmethod
+class Car:
+    count = 0
+    def __init__(self, name):
+        self.name = name
+        Car.count += 1
+
+    @classmethod
+    def outcount(cls):
+        print(cls.count)
+
+pride = Car("프라이드")
+korando = Car("코란도")
+Car.outcount()
+
