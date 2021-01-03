39a6277f3d0305ea55ea653ca66d859f5b9eb399
kimhyeongju <hengzhu1994@gmail.com>
2021-01-03 20:37:48 +0900

    solved!

--- programmers/소수 찾기.py
+++ programmers/소수 찾기.py
@@ -4,24 +4,38 @@
 소수는 1과 자기 자신으로만 나누어지는 수를 의미합니다.
 (1은 소수가 아닙니다.)
 """
+# def solution(n):
+#     answer = []
+#     for i in range(2, n+1):
+#         temp = 0
+#         while True:
+#             for j in range(1,i):
+#                 if i % j == 0:
+#                     temp += 1
+#                 else:
+#                     pass
+#
+#             if temp > 1:
+#                 break
+#             else:
+#                 answer.append(i)
+#
+#     return answer
+
+import numpy as np
 def solution(n):
     answer = []
-    for i in range(2, n+1):
-        temp = 0
-        while True:
-            for j in range(1,i):
-                if i % j == 0:
-                    temp += 1
-                else:
-                    pass
+    arr = []
+    set_arr = []
+    prime = []
 
-            if temp > 1:
-                break
-            else:
-                answer.append(i)
-
-    return answer
+    for i in range(2, round(np.sqrt(n)) + 1):
+        for k in range(i, n//i + 1):
+            arr.append(i*k)
 
 
+    set_arr = set(arr)
+
+    return n - len(set_arr) -1
 a = solution(100)
 print(a)
