d96b261d1839a6ba222d3c515d4ac27d452e6610
kimhyeongju <hengzhu1994@gmail.com>
2021-01-06 21:46:11 +0900

    new

---
+++ programmers/Level 1/정수 제곱근 판별.py
@@ -0,0 +1,15 @@
+"""
+임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+"""
+import numpy as np
+def solution(n):
+    answer = 0
+    x = np.sqrt(n)
+    if x - int(x) == 0:
+        return int((x+1)**2)
+    else:
+        return -1
+
+a = solution(121)
+print(a)
